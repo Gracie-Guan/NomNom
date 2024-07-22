655eb093a3be48629d91094e43d7070aeb91a051
moru628 <WANGMO@tcd.ie>
2024-07-23 00:59:38 +0100

    feature working

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import AppNavigator from './pages/AppNavigator';
+import LoginScreen from './pages/Login';
+import SignUpScreen from './pages/Signup';
+import HomeScreen from './pages/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Navigator">
+      <Stack.Screen name="Navigator" component={AppNavigator} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignUpScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
