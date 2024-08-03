ff16c2b5d013c20e700c9c8ca46f82486cb5e400
Gracie Guan <guanyu@tcd.ie>
2024-08-03 22:31:12 +0100

    Modify the map layout, able to show restaurant card and markers now.

--- client/screens/Map.js
+++ client/screens/Map.js
@@ -1,12 +1,23 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import React, { useContext, useEffect, useState, useRef } from 'react';
+import { View, StyleSheet, Dimensions, Text, TouchableOpacity, Platform, Linking } from 'react-native';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { UserLocation } from '../Context/UserLocation';
-import { Flex } from '@ant-design/react-native';
+import RestaurantCard from '../Components/RestroCards';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const restaurants = [
+// should be all the database restaurants, let's use fake data first
+
+  { id: 1, name: "Joe's Diner", latitude: 53.349805, longitude: -6.260310 },
+  { id: 2, name: "Pizza Place", latitude: 53.348005, longitude: -6.263320 },
+  
+];
 
 export default function GoogleMapsView() {
   const [mapRegion, setMapRegion] = useState(null);
   const { location } = useContext(UserLocation);
+  const [selectedRestro, setSelectedRestro] = useState(null);
+  const mapRef = useRef(null);
   
   useEffect(() => {
     if (location) {
@@ -19,6 +30,35 @@ export default function GoogleMapsView() {
     }
   }, [location]);
 
+  const handleMarkerPress = (restaurant) => {
+    setSelectedRestro(restaurant);
+  };
+
+  const toUserLocation = () => {
+    if (location && mapRef.current) {
+      mapRef.current.animateToRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+      })
+    }
+  };
+
+  const navToRestaurant = () => {
+    if (selectedRestro && location) {
+      const scheme = Platform.select({ios: 'maps:0,0?q=', android:'geo:0,0?q='});
+      const latLng = `${selectedRestro.latitude},${selectedRestro.longitude}`;
+      const label = selectedRestro.name;
+      const url = Platform.select({
+        ios:`${scheme}${label}@${latLng}`,
+        android: `${scheme}${latLng}(${label})`
+      });
+
+      Linking.openURL(url).catch((err)=>console.log('An erro occurred', err));
+    }
+  };
+
   return (
     <View style={styles.container}>
       
@@ -26,8 +66,33 @@ export default function GoogleMapsView() {
           style={styles.map} 
           showsUserLocation={true}
           region={mapRegion}
-        ></MapView>
-      
+        >
+       {restaurants.map((restaurant) => (
+          <Marker
+            key={restaurant.id}
+            coordinate={{latitude: restaurant.latitude, longitude: restaurant.longitude}}
+            onPress={() => handleMarkerPress(restaurant)}
+          >
+            <Callout>
+              <Text>{restaurant.name}</Text>
+            </Callout>
+          </Marker>
+        ))}
+        </MapView>
+        
+        <TouchableOpacity style={styles.goToUser} onPress={toUserLocation}>
+           <Ionicons name="locate" size={24} color="#9e9e9e" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.navToRestro} onPress={navToRestaurant}>
+           <Ionicons name="navigate-outline" size={24} color="#9e9e9e" />
+        </TouchableOpacity>
+
+        {selectedRestro && (
+          <View style={styles.card}>
+            <RestaurantCard layout='list' restaurant={selectedRestro} />
+          </View>
+        )}
 
     </View>
   );
@@ -37,10 +102,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
+  card:{
+    position:'absolute',
+    bottom:20,
+    left:16,
+    right:16,
+  },
+
+  cardTitle:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom:5,
+  },
+
   map: {
     width: Dimensions.get('screen').width,
     height: Dimensions.get('screen').height,
     padding: 10,
+  },
 
-  }
+  goToUser:{
+    position:'absolute',
+    right:16,
+    top:'60%',
+    padding: 10,
+    borderRadius:30,
+    elevation:5,
+    backgroundColor:"#fff",
+  },
+
+  navToRestro:{
+    position:'absolute',
+    right:16,
+    top:'68%',
+    padding: 10,
+    borderRadius:30,
+    elevation:5,
+    backgroundColor:"#fff",
+  },
 });
